4ff0aa940f16c879f25592f11608da85f3da4118
Egor Kirienko <egor.keen@mail.ru>
2022-11-10 16:32:44 +0300

    исправление 2-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,16 @@ let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.form');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-let nameInput = document.querySelector('.form__input_data_name');
-let infoInput = document.querySelector('.form__input_data_info');
+let nameInput = formElement.querySelector('.form__input_data_name');
+let infoInput = formElement.querySelector('.form__input_data_info');
 let userName = document.querySelector('.profile__name');
 let userInfo = document.querySelector('.profile__about');
-
-//Предварительно заносим данные в инпуты из разметки
-nameInput.value = userName.textContent;
-infoInput.value = userInfo.textContent;
 
 //Открытие popup'а
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  infoInput.value = userInfo.textContent;
 }
 
 //Закрытие popup'а
